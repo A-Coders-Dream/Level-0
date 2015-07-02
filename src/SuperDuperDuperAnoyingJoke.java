d450ad300ec89693f6c62c83548ee095c20ef377
Guest User <Guest@WTS6.local>
2015-07-01 20:07:48 -0700

    I made the most annoying joke!!!

---
+++ src/SuperDuperDuperAnoyingJoke.java
@@ -0,0 +1,15 @@
+
+import javax.swing.JOptionPane;
+
+public class SuperDuperDuperAnoyingJoke{
+
+    public static void main(String[] args) {
+	for (int i = 0; i < 4; i++) {
+		JOptionPane.showInputDialog("Knock Knock! (*giggle giggle)");
+		JOptionPane.showInputDialog("Bannana!!!! XD");
+	}
+	JOptionPane.showInputDialog("Knock Knock! (*giggle giggle)");
+	JOptionPane.showInputDialog("ORANGE!!!!!");
+	JOptionPane.showMessageDialog(null, "Orange you glad I didn't say bannana!?");
+     }
+}
